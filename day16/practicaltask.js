c980b1c3e85a557689d3c27922a0ff034371730d
raghu24082001 <raghudae248@gmail.com>
2025-12-08 23:38:15 +0530

    day16

---
+++ day16/practicaltask.js
@@ -0,0 +1,12 @@
+console.log('Raghu')
+
+ //Single line comment
+
+/* Lorem ipsum dolor sit, amet consectetur 
+ adipisicing elit. Reprehenderit quo nobis
+  quod obcaecati, dolores explicabo perspiciatis error aliquid eaque ullam! Consequatur nemo dicta tenetur dolores maiores laudantium! Necessitatibus vel, ipsum, eaque aliquam ratione repellendus reiciendis voluptatum libero distinctio optio sint, adipisci nobis sed quis. Velit quis assumenda maiores aliquam, natus corporis nam? Harum ducimus fugiat consequuntur cupiditate molestiae dolorum! Assumenda alias excepturi provident voluptas necessitatibus? Molestiae facere doloribus, exercitationem delectus maiores deleniti. Soluta amet at illo nihil unde dolorum illum eum, molestias rem deserunt incidunt et nisi ducimus veniam voluptates dolores esse quasi distinctio nam laboriosam quos? Repellat, laudantium aliquam.
+*/
+
+function On(){
+    alert('Button is pressed')
+}
