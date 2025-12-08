62f7296ea46e86d31142fd74de1699e9a2b6cf84
raghu24082001 <raghudae248@gmail.com>
2025-12-08 22:31:53 +0530

    day13

---
+++ JavaScript/Day1/index.js
@@ -0,0 +1,12 @@
+
+function das(){
+   const id= document.getElementById('hi')
+   id.innerHTML='bye'
+
+}
+function day(){
+   const id= document.getElementById('hi')
+   id.innerHTML='supre'
+    id.style.transform='rotatez(360deg)'
+
+}
