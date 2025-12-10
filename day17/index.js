188f86cdfd2aae75c57c20a5bdc8b1194d35281c
raghu24082001 <raghudae248@gmail.com>
2025-12-10 22:43:23 +0530

    Day17

--- day17/index.js
+++ day17/index.js
@@ -1,8 +1,156 @@
+//Declare variables using var, let, and const
 
+// var a=5;
 
-let i = 1;
+// let b=3;
 
-while (i <= 5) {
-    console.log(i);
-    i++;
-}
+// const c=4;
+
+//Check types of: "Hello", 45, true, null.
+
+// console.log(typeof 'hello')
+
+// console.log(typeof 45)
+
+// console.log(typeof true)
+
+// console.log(typeof null)
+
+//Use arithmetic operators to calculate sum, product, and remainder.
+
+// let a=10;
+
+// let b=5;
+
+// console.log(a+b);
+
+// console.log(a-b);
+
+// console.log(a*b);
+
+// console.log(a/b);
+
+// console.log(a%b);
+
+//Compare values using both == and ===
+// let a=4;
+
+// let b='4';
+
+// console.log(a==b);
+// console.log(a===b);
+
+//Write an if-else to check if a number is even or odd.
+// let a=2;
+
+// if(a%2==0){
+//        console.log('even num')
+// }
+// else{
+//     console.log('odd num')
+// }
+
+//Age Checker
+//Input age → output Child/Adult/Senior.
+
+// let a=30;
+
+// if(a>=0 &&  a<=11){
+//     console.log('child')
+// }
+// else if(a>=12 &&  a<=18){
+// console.log('adult')
+// }
+// else{
+//     console.log('senior')
+// }
+
+// Simple Calculator
+// Use operators to add, subtract, multiply, divide two numbers
+
+// let a=10;
+// let b=2;
+// let symbol='-'
+
+// switch(symbol){
+//     case '+':
+//     console.log(a+b)
+//     break
+//     case '-':
+//     console.log(a-b)
+//     break
+//     case '*':
+//     console.log(a*b)
+//     break
+//     case '/':
+//     console.log(a/b)
+//     break
+//     case '%':
+//     console.log(a%b)
+//     break
+
+// // }
+
+// Login Logic
+// If username and password match → print Welcome 
+
+// let username='athi'
+// let password=1234
+
+// let userinput='athi'
+// let passinput=1234
+
+// if( username===userinput && password===passinput){
+//       console.log('authorized person')
+// }
+// else{
+//     console.log('something is wrong')
+// }
+// Student Grading System
+// Use conditions to return A/B/C/D/F based on marks
+
+// let mark=10
+
+// if(mark>=80 && mark<=100){
+//     console.log('A')
+// }
+// else if(mark>=71 && mark<=79){
+//     console.log('B')
+// }
+// else if(mark>=61 && mark<=69){
+//     console.log('C')
+// }
+// else if(mark>=51 && mark<=59){
+//     console.log('D')
+// }
+// else{
+//     console.log('F')
+// }
+
+// let day=6
+
+// switch(day){
+//      case 1:
+//      console.log('Monday')
+//      break;
+//      case 2:
+//      console.log('Tuesday')
+//      break;     
+//      case 3:
+//      console.log('Wednesday')
+//      break;  
+//      case 4:
+//      console.log('Thursday')
+//      break;  
+//      case 5:
+//      console.log('Friday')
+//      break;  
+//      case 6:
+//      console.log('Saturday')
+//      break;  
+//      case 7:
+//      console.log('Sunday')
+//      break;  
+//      default:
+//      console.log('Not exist')
+// }
---
+++ day18/index.js
@@ -0,0 +1,26 @@
+// calulation
+// button
+//validations
+//repeat a task
+
+//Function expression
+//function stored in a variable is called function expression
+
+//arrow funtion
+
+//no function 
+//automatically retuns for single-line expression
+//does not have its own (this) will not work important in es6
+
+//callback
+
+//event handling
+//async programing
+//timers
+//array,method
+//key points
+//js treats function ass first class citizens
+//function can be stores, passed , returned
+
+//function are hoisted
+//arrow function not hoisted
