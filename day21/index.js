7083bc1b38a891e5d8b11d91d2acb3a47be74cbc
raghu24082001 <raghudae248@gmail.com>
2026-01-01 18:32:58 +0530

    day21

---
+++ day21/index.js
@@ -0,0 +1,57 @@
+// const color = ['blue','red','yellow','pink','black']
+
+// console.log(color[0])
+// console.log(color[color.length-1])
+
+// const number = [1,2,3,4,5,6,7,8]
+// number.pop()
+// console.log(number)
+
+// number.push(8)
+// console.log(number)
+
+// number.shift()
+// console.log(number)
+
+// number.unshift(1)
+// console.log(number)
+
+// const number = [1,2,3,4,5]
+
+// number.forEach(n=>console.log(n))
+
+// const Snumber= number.map(n=>n*n)
+
+// console.log(Snumber)
+
+// const Gnum = [20,50,60,40]
+
+// console.log(Gnum.filter(n=>n>50))
+
+// const num = [2,3,4,5,6]
+
+// console.log(num.reduce((a,b)=>a=a+b))
+
+// const age = [15,20,22,25,17]
+
+// console.log(age.filter(n=>n>18))
+
+// const a=[2,3,4]
+
+// const b=[5,6,7]
+
+// const combine= a.concat(b)
+
+// console.log(combine)
+
+// const com = [...a,...b]
+
+// console.log(com)
+
+
+
+
+
+
+
+
