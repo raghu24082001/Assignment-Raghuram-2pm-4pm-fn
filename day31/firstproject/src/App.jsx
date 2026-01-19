e83f15b097adbd80801f1dda386648b1e2c93b63
raghu24082001 <raghudae248@gmail.com>
2026-01-19 21:58:57 +0530

    day31

---
+++ day31/firstproject/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  document.title= 'react     -app'
+  const role='front end'
+  return (
+    <>
+    <div>Raghuram</div>
+    <div>Lorem ipsum dolor sit {role}.</div>
+    </>
+  )
+}
+
+export default App
