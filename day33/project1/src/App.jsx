8768c33f781f644b14ffcbbf4f1c9c64811ee26f
raghu24082001 <raghudae248@gmail.com>
2026-02-03 19:58:37 +0530

    day33

---
+++ day33/project1/src/App.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [count, setCount] = useState(0)
+  const [text,changeText] = useState('Hi')
+  const [value,setValue] = useState('')
+  const [theme,setTheme] = useState(true)
+
+  function add() {
+    setCount(prev => prev + 1)
+  }
+
+  function sub(){
+     setCount(prev => prev - 1)
+  }
+  function change(){
+    changeText('Bye')
+
+  }
+  function changeagain(){
+    changeText('hi')
+  }
+
+  function reset(){
+    setCount(0)
+   
+  }
+
+  function handlechange(e){
+    setValue(e.target.value)
+  }
+
+
+  return (
+    <div style={{background:theme ?'white' : 'black',
+      color:theme ?'black' : 'white',
+      height:'100vh',
+      width:'100vw',
+      textAlign:'center',
+
+    }}>
+      <button onClick={()=>setTheme(!theme)}>{theme ? 'dark' : 'light'}</button><br />
+
+      <p >Hello There</p>
+
+       <button onClick={sub}>-</button>
+
+      <p>{count}</p>
+      <button onClick={add}>+</button><br />
+      <button onClick={reset}>Reset</button>
+     <p >{text}</p> 
+     <button onClick={change} onDoubleClick={changeagain}>Toggle</button>
+     <form action="">
+      <input type="text"
+        onChange={handlechange}
+       />
+      <p>{value}</p>
+     </form>
+
+    </div>
+  )
+}
+
+export default App
